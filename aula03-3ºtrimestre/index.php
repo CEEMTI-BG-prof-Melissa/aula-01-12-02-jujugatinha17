51b8aa3237a75e50073a47d0dbfcf0af123e3d8c
Juliana <juliana.hcampos@aluno.edu.es.gov.br>
2025-09-09 11:56:32 -0300

    Create index.php

---
+++ aula03-3ºtrimestre/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Aula 03-PHP</title>
+    </head>
+    <body>
+         <?php
+             $nome="Juliana";
+
+             $n1=10;
+             $n2=2;
+
+             $soma=$n1+$n2;
+
+             echo "Meu nome é ".$nome ;
+             echo "<br> Meu nome é $nome";
+             echo"<br><br> A soma de $n1 com $n2 é igual a $soma.";
+         ?>
+    </body>
+</html>
