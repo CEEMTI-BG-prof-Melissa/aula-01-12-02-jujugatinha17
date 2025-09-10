30c0f64d788522f452c601aabb128b8784ee6537
Juliana <juliana.hcampos@aluno.edu.es.gov.br>
2025-09-10 11:46:42 -0300

    Update index.php

--- aula03-3ºtrimestre/index.php
+++ aula03-3ºtrimestre/index.php
@@ -14,6 +14,10 @@
              echo "Meu nome é ".$nome ;
              echo "<br> Meu nome é $nome";
              echo"<br><br> A soma de $n1 com $n2 é igual a $soma.";
+             echo"<br><br>A subtração dos números $n1 e $n2 é igual a: " .($n1-$n2).".";
+             echo"<br><br>A divisão dos números $n1 e $n2 é igual a: ".($n1/$n2).".";
+             echo"<br><br>A multiplicação dos números $n1 e $n2 é igual a: ".($n1*$n2).".";
+             echo"<br><br>O resto da divisão dos números $n1 e $n2 é igual a: ".($n1%n2).".";
          ?>
     </body>
 </html>
