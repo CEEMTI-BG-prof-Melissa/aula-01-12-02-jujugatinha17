56414aa7af31bd60e4ecb6c2e73550c8de5c8e36
Juliana <juliana.hcampos@aluno.edu.es.gov.br>
2025-09-10 11:50:44 -0300

    Update index.php

--- aula03-3ºtrimestre/index.php
+++ aula03-3ºtrimestre/index.php
@@ -17,7 +17,7 @@
              echo"<br><br>A subtração dos números $n1 e $n2 é igual a: " .($n1-$n2).".";
              echo"<br><br>A divisão dos números $n1 e $n2 é igual a: ".($n1/$n2).".";
              echo"<br><br>A multiplicação dos números $n1 e $n2 é igual a: ".($n1*$n2).".";
-             echo"<br><br>O resto da divisão dos números $n1 e $n2 é igual a: ".($n1%n2).".";
+             echo"<br><br>O resto da divisão dos números $n1 e $n2 é igual a: ".($n1%$n2).".";
          ?>
     </body>
 </html>
